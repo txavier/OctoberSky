16bd4529b232b58fea3c5db6d2e7b9c5de84bfe0
txavier <txavier@gmail.com>
2014-12-26 20:33:15 -0500

    - Project Core, Infrastructure and test projects created.

---
+++ StuffFinder.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StuffFinder.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
