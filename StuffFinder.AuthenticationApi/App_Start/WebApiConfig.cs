d1a4eceeafc7c2a27d4a8d92d48da9c92923e48e
txavier <txavier@gmail.com>
2015-01-01 18:12:05 -0500

    - Fixed code that stopped this branch from compiling.

--- StuffFinder.AuthenticationApi/App_Start/WebApiConfig.cs
+++ StuffFinder.AuthenticationApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace StuffFinder.AuthenticationApi
@@ -19,6 +21,10 @@ namespace StuffFinder.AuthenticationApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
+
     }
 }
--- StuffFinder.CompositionRoot/DefaultRegistry.cs
+++ StuffFinder.CompositionRoot/DefaultRegistry.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using StructureMap.Configuration.DSL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StructureMap.Graph;
+using StructureMap.Web;
+using System.Data.Entity;
+using AutoClutch.Auto.Service.Services;
+using AutoClutch.Auto.Service.Interfaces;
+using AutoClutch.Auto.Repo.Interfaces;
+using AutoClutch.Auto.Repo.Objects;
 
 namespace StuffFinder.CompositionRoot
 {
-    public class DefaultRegistry
+    public class DefaultRegistry : Registry
     {
         public DefaultRegistry()
         {
@@ -17,19 +25,19 @@ namespace StuffFinder.CompositionRoot
                     scan.WithDefaultConventions();
                 });
 
-            For<DbContext>().HybridHttpOrThreadLocalScoped().Use<LiteratureAssistantDbContext>();
+            //For<DbContext>().HybridHttpOrThreadLocalScoped().Use<LiteratureAssistantDbContext>();
 
             For(typeof(IService<>)).Use(typeof(Service<>));
 
             For(typeof(IRepository<>)).Use(typeof(Repository<>));
 
-            For<IItemService>().Use<ItemService>();
+            //For<IItemService>().Use<ItemService>();
 
-            For<IUserService>().Use<UserService>();
+            //For<IUserService>().Use<UserService>();
 
-            For<IOrderService>().Use<OrderService>();
+            //For<IOrderService>().Use<OrderService>();
 
-            For<ICountService>().Use<CountService>();
+            //For<ICountService>().Use<CountService>();
 
             //For<IUserStore<MyUser>>().Use<MyUserStore>();
 
@@ -37,21 +45,21 @@ namespace StuffFinder.CompositionRoot
 
             //For<UserManager<MyUser>>().Use<MyUserManager>();
 
-            For(typeof(IUserStore<>)).Use(typeof(UserStore<>));
+            //For(typeof(IUserStore<>)).Use(typeof(UserStore<>));
 
-            Policies.SetAllProperties(prop => prop.OfType<IService<item>>());
+            //Policies.SetAllProperties(prop => prop.OfType<IService<item>>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IService<itemAttribute>>());
+            //Policies.SetAllProperties(prop => prop.OfType<IService<itemAttribute>>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IService<templateAttribute>>());
+            //Policies.SetAllProperties(prop => prop.OfType<IService<templateAttribute>>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IItemService>());
+            //Policies.SetAllProperties(prop => prop.OfType<IItemService>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IUserService>());
+            //Policies.SetAllProperties(prop => prop.OfType<IUserService>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IOrderService>());
+            //Policies.SetAllProperties(prop => prop.OfType<IOrderService>());
 
-            Policies.SetAllProperties(prop => prop.OfType<IUserStore<ApplicationUser>>());
+            //Policies.SetAllProperties(prop => prop.OfType<IUserStore<ApplicationUser>>());
         }
     }
 }
