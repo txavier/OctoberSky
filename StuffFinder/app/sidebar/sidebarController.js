198e6bd9c4aed6e87902047ee89d1b53662b66f8
txavier <txavier@gmail.com>
2015-01-24 12:37:08 -0500

    - Added feature to see the username in the sidebar.  - Added logout from the sidebar.

---
+++ StuffFinder/app/sidebar/sidebarController.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+app.controller('sidebarController', ['$scope', 'authService', function ($scope, authService) {
+
+    $scope.authentication = {};
+
+    $scope.authentication.userName = authService.authentication.userName;
+
+    var logout = function () {
+        authService.logOut();
+    }
+
+    $(document).ready(function () {
+
+        $(".player").mb_YTPlayer();
+
+    });
+}]);
