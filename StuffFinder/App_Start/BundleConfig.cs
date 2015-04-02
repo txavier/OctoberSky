aa5b71399c2d4ba1680d8391ad2984573cd0d09b
txavier <txavier@gmail.com>
2015-04-01 21:39:26 -0400

    - Version 1.3.4.0.  - Optimized css and js minification bundling.  - Replaced nationality typeahead with dropdown.

--- StuffFinder/App_Start/BundleConfig.cs
+++ StuffFinder/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace StuffFinder
 {
@@ -29,26 +28,65 @@ namespace StuffFinder
             bundles.Add(new ScriptBundle("~/bundles/ngToast").Include(
                 "~/Scripts/vendor/ngToast/ngToast.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/angular").IncludeDirectory(
-                "~/app", "*Controller.js", true));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/app/home/homeController.js",
+                "~/app/things/thingsController.js",
+                "~/app/start/startController.js",
+                "~/app/finding/foundItController.js",
+                "~/app/sidebar/sidebarController.js",
+                "~/app/whereIsIt/whereIsItController.js",
+                "~/app/things/thingController.js",
+                "~/app/admin/jumbotronYoutubeVideoSettingsController.js",
+                "~/app/search/searchController.js",
+                "~/app/things/editThingController.js",
+                "~/app/finding/editFindingController.js",
+                "~/app/finding/foundThingAndLocationController.js",
+                "~/app/feedback/feedbackController.js",
+                "~/app/users/userProfileController.js",
+                "~/app/services/dataService.js",
+                "~/app/things/thingsService.js",
+                "~/app/services/votesService.js",
+                "~/app/services/me2Service.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/nya-bs-select").Include(
                 "~/Scripts/vendor/nya-bootstrap-select/dist/js/nya-bs-select.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/ngToast").Include(
-                "~/Content/ngToast.css",
-                "~/Content/ngToast-animations.css"));
+            //bundles.Add(new StyleBundle("~/Content/ngToast").Include(
+            //    "~/Content/ngToast.css",
+            //    "~/Content/ngToast-animations.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.min.css",
                       "~/Content/styles.css"));
 
-            bundles.Add(new StyleBundle("~/Content/nya-bootstrap-select").Include(
-                "~/Content/nya-bootstrap-select/dist/css/nya-bs-select.css"));
+            bundles.Add(new StyleBundle("~/Content/site").Include(
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/thirdParty").Include(
+                "~/Content/ngToast.css",
+                "~/Content/ngToast-animations.css",
+                "~/Content/nya-bs-select.min.css",
+                "~/Content/loading-bar.css",
+                "~/Content/textAngular.css",
+                "~/Content/social-buttons.css"
+                ));
+
+            //bundles.Add(new StyleBundle("~/Content/nya").Include(
+            //    "~/Content/nya-bs-select.min.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/loadingBar").Include(
+            //    "~/Content/loading-bar.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/textAngular").Include(
+            //    "~/Content/textAngular.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/socialButtons").Include(
+            //    "~/Content/social-buttons.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            //BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
