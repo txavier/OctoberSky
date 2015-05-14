1e76d63a0210fbd4528d13ee51e35ea0a2d4bcb1
txavier <txavier@gmail.com>
2015-05-14 00:44:14 -0400

    fix the bug that was causing DELETE and PUT web actions not to hit the web api in the resource server

--- StuffFinder.ResourceServer/App_Start/WebApiConfig.cs
+++ StuffFinder.ResourceServer/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace StuffFinder.ResourceServer.App_Start
 {
@@ -12,6 +13,9 @@ namespace StuffFinder.ResourceServer.App_Start
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("http://deltanovember.xaviersoftware.com", "*", "*");
+            config.EnableCors(cors);
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
